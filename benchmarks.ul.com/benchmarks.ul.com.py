e2567b93226c24974781c448190e9af0897839eb
DESKTOP-D2V1UJK\Danilshik <danilshik@yandex.ru>
2018-12-02 08:07:57 +0300

    Удалены не нужные методы, добавлены методы по нахождению оптимальных данных

--- benchmarks.ul.com/benchmarks.ul.com.py
+++ benchmarks.ul.com/benchmarks.ul.com.py
@@ -1,21 +1,22 @@
-import os
+
 import time
 import logging
 import lxml.html
-from collections import OrderedDict
-import csv
 import requests
+import re
 
-from contextlib import suppress
 from mysql.connector import MySQLConnection, Error
 
 log = logging.getLogger(__name__)
 format = '%(asctime)s %(levelname)s:%(message)s'
 logging.basicConfig(format=format, level=logging.INFO)
 
+list_database_product = None
+change_list_database = True
 count_add = 0
 no_count_add = 0
-
+list_many_optimization = []
+count_many_optimization = 0
 db_bd = "hardware"
 db_user = "root"
 db_password = ""
@@ -34,7 +35,7 @@ id_brenchmark_cpu = ""
 gpus = []
 cpus = []
 
-name_list = []
+
 #Костыль, предназначен для удаления названия найденного блока, чтобы сохранить, например список с Html-тегами
 def to_string(node):
     return lxml.html.tostring(node, encoding='unicode')
@@ -67,14 +68,16 @@ def select_database(sql, data):
         conn = MySQLConnection(user = db_user, password = db_password, host = db_host, database = db_bd)
         conn.autocommit = True
         cursor = conn.cursor()
-        cursor.execute(sql, data)
+        if (data != None):
+            cursor.execute(sql, data)
+        else:
+            cursor.execute(sql)
         rows = cursor.fetchall()
         cursor.close()
         conn.close()
         return rows
     except Error as e:
         print("Ошибка:", e)
-        exit(0)
 
 def insert_database(sql, data):
     try:
@@ -87,7 +90,8 @@ def insert_database(sql, data):
         conn.close()
         return lastrowid
     except Error as e:
-        print("Ошибка:", e)
+        print("Ошибка:", e, data)
+        exit(0)
 
 def update_database(sql, data):
     try:
@@ -105,7 +109,6 @@ def update_database(sql, data):
 def get_cpu_benchmark_id(name):
     sql_query = "SELECT id FROM cpu_benchmark WHERE name = %s"
     data = (name,)
-    sql_query = "INSERT INTO cpu_benchmark (name) VALUES(%s)"
     rows = select_database(sql_query, data)
     if(len(rows)==1):
         return rows[0][0]
@@ -155,35 +158,23 @@ def add_cpu_benchmark_multi_thread(cpu_id, id_benchmark, multi_thread):
                     sql_query = "INSERT INTO cpu_benchmark_value(cpu_id, cpu_benchmark_id, multi_core_value) VALUES (%s, %s, %s)"
                     data = (cpu_id, id_brenchmark_cpu, multi_thread)
                     return insert_database(sql_query, data)
-def get_cpu_database():
-    sql_query = "SELECT name,id FROM cpu"
-    try:
-        conn = MySQLConnection(user = db_user, password = db_password, host = db_host, database = db_bd)
-        conn.autocommit = True
-        cursor = conn.cursor()
-        cursor.execute(sql_query)
-        rows = cursor.fetchall()
-        cursor.close()
-        conn.close()
-        return rows
-    except Error as e:
-        print("Ошибка:", e)
-        exit(0)
 
-def get_gpu_database():
-    sql_query = "SELECT name, id FROM gpu "
-    try:
-        conn = MySQLConnection(user = db_user, password = db_password, host = db_host, database = db_bd)
-        conn.autocommit = True
-        cursor = conn.cursor()
-        cursor.execute(sql_query)
-        rows = cursor.fetchall()
-        cursor.close()
-        conn.close()
-        return rows
-    except Error as e:
-        print("Ошибка:", e)
-        exit(0)
+def get_product_database(type):
+    global id_brenchmark_gpu, id_brenchmark_cpu
+    if(type == "gpu"):
+        sql_query = "SELECT gpu.id, gpu.name from gpu LEFT JOIN gpu_benchmark_value ON gpu.id = gpu_benchmark_value.gpu_id WHERE gpu_benchmark_value.gpu_benchmark_id != %s OR gpu_benchmark_value.gpu_benchmark_id is null"
+        data = (id_brenchmark_gpu, )
+    if(type == "cpu"):
+        sql_query = "SELECT cpu.id, cpu.name from cpu LEFT JOIN cpu_benchmark_value ON cpu.id = cpu_benchmark_value.cpu_id WHERE cpu_benchmark_value.cpu_benchmark_id != %s OR cpu_benchmark_value.cpu_benchmark_id is null"
+        data = (id_brenchmark_cpu,)
+
+    rows = select_database(sql_query, data)
+    return rows
+
+
+
+
+
 
 def add_gpu_benchmark(gpu_id, id_benchmark, mark):
 
@@ -197,7 +188,10 @@ def add_gpu_benchmark(gpu_id, id_benchmark, mark):
             sql_query = "INSERT INTO gpu_benchmark_value (gpu_id, gpu_benchmark_id, value) VALUES (%s, %s, %s)"
             return insert_database(sql_query, data)
 
+
+
 def get_product_id(name, type):
+    global list_database_product
     name_text = name
     if(type == "cpu"):
         sql_query = "SELECT id FROM cpu WHERE name = %s"
@@ -209,8 +203,7 @@ def get_product_id(name, type):
     rows = select_database(sql_query, data)
     if(len(rows) == 1):
         return rows[0][0]
-def search_best():
-    print()
+
 def parse(url):
     global  count_add, no_count_add
     r = request(url)
@@ -234,57 +227,146 @@ def parse(url):
                 count_add += 1
                 add_gpu_benchmark(id_product, id_brenchmark_gpu, value)
             else:
-                no_count_add += 1
-                gpus.append([name, value])
+                id_product = search_best_conformity(name, type)
+                if(id_product != None):
+                    count_add += 1
+                    add_gpu_benchmark(id_product, id_brenchmark_gpu, value)
+
+                else:
+                    no_count_add += 1
+                    gpus.append([name, value])
         else:
             if(type == "cpu"):
-                # id_product = get_product_id(name, type)
-                # if (id_product != None):
-                #     count_add += 1
-                #
-                # else:
-                #     no_count_add += 1
-                # print(name)
-                cpus.append([name, value])
+                id_product = get_product_id(name, type)
+                if (id_product != None):
+                    count_add += 1
+                    add_cpu_benchmark_multi_thread(id_product, id_brenchmark_cpu, value)
+                else:
+                    id_product = search_best_conformity(name, type)
+                    if (id_product != None):
+                        count_add += 1
+                        add_cpu_benchmark_multi_thread(id_product, id_brenchmark_cpu, value)
+                    else:
+                        no_count_add += 1
+                        cpus.append([name, value])
+
+def optimization(item):
+    if(item.find("NVIDIA GeForce GTX 745 OEM") != -1):
+        item = "NVIDIA GeForce GTX 745"
+    if(item.find("AMD Radeon HD 7470 OEM") != -1):
+        item = "AMD Radeon HD 7470"
+    if (item.find("Intel Core i5 Processor I5-750") != -1):
+        item = "Intel Core i5-750"
+    if(item.find("(Desktop)") != -1):
+        item = item.replace("(Desktop)", "")
+    text = re.search("Graphics$", item)
+    if (text != None):
+        item = item.replace(text.group(0), "")
+    text = re.search("Mobile[)]$", item)
+    if (text != None):
+        item = item.replace(text.group(0), "")
+    text = re.search("Mobile$", item)
+    if (text != None):
+        item = item.replace(text.group(0), "")
+    if(item.find("(Notebook)") != -1):
+        item = item.replace("(Notebook)", "(Laptop)")
+    if (item.find("DDR3") != -1):
+        item = item.replace("DDR3", "")
+    if (item.find("GHz Edition") != -1):
+        item = item.replace("GHz Edition", "")
+
+    #cpu
+
+    if(item.find("Extreme Edition Processor") != -1):
+        item = item.replace("Extreme Edition Processor", "")
+    item1 = item
+    if (item.find("Processor") != -1):
+        item = item.replace("Processor", "")
+    if (item.find("Gold") != -1):
+        item = item.replace("Gold", "")
+
+    if (item.find("-") != -1):
+        item = item.replace("-", "") #?
+    if (item.find(" ") != -1):
+        item = item.replace(" ", "")
+    if (item.find("(") != -1):
+        item = item.replace("(", "")
+    if (item.find(")") != -1):
+        item = item.replace(")", "")
+
+    item = item.lower()
+    if (item.find("gtx") != -1):
+        item = item.replace("gtx", "")
+    return item
+
+def search_best_conformity(product, type):
+    global list_many_optimization
+    global change_list_database, list_database_product
+    if(change_list_database):
+        list_database_product = get_product_database(type)
+        change_list_database = False
+
+    name_product_list = [i[1] for i in list_database_product]
+    index_product_list = [i[0] for i in list_database_product]
+    list_new = []
+    for name in name_product_list:
+        list_new.append(optimization(name))
+    new_product = optimization(product)
+    count_optimization = 0
+    memory_index = -1
+    many_optimization = []
+    for index, new in enumerate(list_new):
+        if(new == new_product):
+            many_optimization.append(name_product_list[index])
+            count_optimization += 1
+            memory_index = index
+    if(count_optimization == 1):
+        print("Оптимизировалось:", name_product_list[memory_index], index_product_list[memory_index])
+        change_list_database = True
+        return index_product_list[memory_index]
+    if(count_optimization > 0):
+        global count_many_optimization
+        count_many_optimization+= 1
+        list_many_optimization.append(many_optimization)
 
 
-def get_database_gpus():
-    sql = "SELECT gpu.id, gpu.name FROM gpu, gpu_benchmark_value WHERE gpu.id = gpu_becnhmark_value.gpu_id"
+
+
 
 def main():
-    global cpus
+
+    global cpus, id_brenchmark_gpu, id_brenchmark_cpu, list_many_optimization, count_many_optimization, count_add, no_count_add, change_list_database
+    start_time = time.time()
+    id_brenchmark_gpu = get_gpu_benchmark_id("3D Mark", "Graphics Score")
+    id_brenchmark_cpu = get_cpu_benchmark_id("3D Mark Physics Score")
+    print("Идентификатор бенчмаркета:", id_brenchmark_gpu)
+    print("Идентификатор бенчмаркета:", id_brenchmark_cpu)
     for url_main in urls_main:
+        change_list_database = True
         parse(url_main)
-    print("Количество add:", count_add)
-    print("Количество no_add:", no_count_add)
-    # cpus_name = []
-    # for cpu in cpus:
-    #     cpus_name.append(cpu[0])
-    #
-    # # print(cpus_name)
-    #
-    # cpus_database = get_cpu_database()
-    # test = []
-    #
-    # for cpu_database in cpus_database:
-    #     # print(cpus_database[0])
-    #     test.append(cpu_database[0])
-    # print(test)
-    #     products = print(max(process.extract(cpu_database[0], cpus_name)))
-    # with open("benchmark_gpu.net.txt", "w", newline="") as file:
-    #     writer = csv.writer(file)
-    #     writer.writerows(gpus)
-    # with open("benchmark_cpu.net.txt", "w", newline="") as file:
-    #     writer = csv.writer(file)
-    #     writer.writerows(cpus)
 
-    # gpus_database = get_gpu_database()
-    # test = []
 
-    # for gpu_database in gpus_database:
-    #     print(gpu_database[0], "-", difflib.get_close_matches(gpu_database[0], gpus_name))
-        # products = print(gpu_database[0], "-", max(process.extract(gpu_database[0], gpus_name)))
-    # print(test)
+        print("Количество add:", count_add)
+        print("Количество no_add:", no_count_add)
+        print("Количество 2 и более значений:", count_many_optimization)
+        for list_many_optimization in list_many_optimization:
+            print(list_many_optimization)
+
+        count_add = 0
+        count_many_optimization = 0
+        no_count_add = 0
+    print("------------------------Недобавленные  GPUS-----------------------------")
+    for gpu in gpus:
+        print(gpu)
+    print("------------------------Недобавленные  CPUS-----------------------------")
+    for cpu in cpus:
+        print(cpu)
+    print("Время работы:", time.time() - start_time)
+
+
+
+
+
 
 
 
@@ -292,15 +374,6 @@ def main():
 
 
 if __name__ == '__main__':
-    start_time = time.time()
-    id_brenchmark_gpu = get_gpu_benchmark_id("3D Mark", "Graphics Score")
-    print(id_brenchmark_gpu)
-    # id_brenchmark_cpu = get_cpu_benchmark_id("3DMark Physics Score")
+
 
     main()
-    for gpu in gpus:
-        print(gpu)
-
-
-
-    print("Время работы:", time.time() - start_time)
